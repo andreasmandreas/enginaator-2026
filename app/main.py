5b47b0f015dc99ad2978072b54a247be87e2bd86
Stepan Denysenko <60338342+stepden04@users.noreply.github.com>
2026-04-17 03:54:59 +0300

    feat: implemented placeholder endpoints, added db interaction methods, refactor, reformat

--- app/main.py
+++ app/main.py
@@ -3,16 +3,13 @@ from pathlib import Path
 from uuid import uuid4
 
 import whisper
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, Request, UploadFile
+
+from db import SvaraDB
 
 model = whisper.load_model("base")
 app = FastAPI(title="SVARA Room Service API")
-
-# app.include_router(requests_router, prefix="/api/requests", tags=["requests"])
-# app.include_router(inventory_router, prefix="/api/inventory", tags=["inventory"])
-# app.include_router(reports_router, prefix="/api/reports", tags=["reports"])
-# app.include_router(reconciliations_router, prefix="/api/reconciliations", tags=["reconciliations"])
-# app.include_router(ws_router, tags=["websocket"])
+db_instance = SvaraDB("svara", "svara", "iamstupid123", "172.28.61.160")
 
 
 @app.get("/api/health")
@@ -20,23 +17,59 @@ def health() -> dict[str, str]:
     return {"status": "ok"}
 
 
-@app.post("/api/requests")
-def new_request(): ...
+@app.post("/api/new_request")
+async def new_request(request: Request) -> tuple[dict[str, typing.Any], int]:
+    room_nr = request.query_params.get("room_nr")
+    if not room_nr:
+        return {"error": "Missing room_nr"}, 400
+
+    data = await request.body()
+    if not data:
+        return {"error": "Missing audio file."}, 400
+
+    file = Path("tmp", mkdir_exist_ok=True).joinpath(f"{uuid4().hex}.webm")
+    try:
+        with file.open("wb") as buffer:
+            buffer.write(data)
+
+    except Exception as e:  # noqa: BLE001
+        return {"error": str(e)}, 500
+
+    finally:
+        file.unlink()
+
+    text = model.transcribe(file.as_posix())
+
+    # TODO feed into LLM
+    {"text": text, "room_nr": room_nr, "items": ...}  # noqa: B018
+    return None
 
 
-@app.get("/api/requests")
-def get_requests(): ...
+@app.get("/api/all_requests")
+async def get_requests() -> list[Request]:
+    return await db_instance.get_requests()
 
 
 @app.get("/api/requests/room/<room>")
-def get_requests_by_room(room: str): ...
+def get_requests_by_room(room: str) -> None: ...
 
 
-@app.patch("/api/requests/<request_id>")
-def update_request(request_id: str): ...
+@app.put("/api/requests/<request_id>")
+async def update_request(request: Request, request_id: str) -> None:
+    eta = (await request.json()).get("eta")
+    if eta:
+        int(eta)
+    status = (await request.json()).get("status")
+    if status:
+        status = status.upper()
+
+    await db_instance.update_request(
+        int(request_id),
+        eta if eta else None,
+        status if status else None,
+    )
 
 
-@app.post("/api/voice_to_text")
 def voice_to_text(files: UploadFile) -> dict[str, str | list[typing.Any]]:
     filename = files.filename or f"{uuid4().hex}.mp3"
 
@@ -46,7 +79,7 @@ def voice_to_text(files: UploadFile) -> dict[str, str | list[typing.Any]]:
         buffer.write(files.file.read())
     try:
         return model.transcribe(audio=file.as_posix())
-    except Exception as e:
+    except Exception as e:  # noqa: BLE001
         return {"error": str(e)}
     finally:
         file.unlink()
